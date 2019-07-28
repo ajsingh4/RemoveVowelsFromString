b112f47d05e09c10df9c40ed139987f133514b66
Aj Gill <ajgill@ucdavis.edu>
2019-07-28 16:31:41 -0700

    Initial Commit

---
+++ RemoveVowelsFromString/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RemoveVowelsFromString
+//
+//  Created by Aj Gill on 7/28/19.
+//  Copyright © 2019 Aj Gill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
