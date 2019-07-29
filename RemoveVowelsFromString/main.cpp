40630edd8877112a194d99eb4055474b00f64c99
Aj Gill <ajgill@ucdavis.edu>
2019-07-28 20:15:16 -0700

    Initial approach using two for loops, some if statements, and a stack.

--- RemoveVowelsFromString/main.cpp
+++ RemoveVowelsFromString/main.cpp
@@ -7,10 +7,54 @@
 //
 
 #include <iostream>
+#include <stack>
 
 //For ease of access
 using namespace std;
 
-int main() {
+bool checkForVowels(char* A){
+    if(*A == 'A' || *A == 'A'){
+        return true;
+    }
+    else if(*A == 'E' || *A == 'e'){
+        return true;
+    }
+    else if(*A == 'I' || *A == 'i'){
+        return true;
+    }
+    else if(*A == 'O' || *A == 'o'){
+        return true;
+    }
+    else if(*A == 'U' || *A == 'u'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
+string removeVowels(string word){
+    string answer;
+    //Initialize a stack to utilize LIFO
+    stack<char> reverser;
+    for(int i=0; i<word.length(); i++){
+        //If the letter is not a vowel, it is added to the stack
+        if(!checkForVowels(&word[i])){
+            reverser.push(word[i]);
+        }
+    }
+    int size = reverser.size();
+    //Go through the stack elements and get characters in the reverse order
+    for(int j=0; j<size; j++){
+        answer += reverser.top();
+        reverser.pop();
+    }
+    return answer;
+}
+
+int main() {
+    string word = "AIRPLANE";
+    string finalAnswer = removeVowels(word);
+    cout << "Initial word: " << word;
+    cout << "\nWord reversed without vowels: " << finalAnswer << endl;
 }
