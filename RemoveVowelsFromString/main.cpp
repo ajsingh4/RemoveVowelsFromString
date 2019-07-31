d0e1612071d62259bfed648cff707002e70dcadc
Aj Gill <ajgill@ucdavis.edu>
2019-07-30 17:39:21 -0700

    Solution refined to reversal and removal of vowels in place. Used a set to keep track of the indices of vowels as it automatically sorts the inserted values. As the loop goes through the string, it keeps the vowels clumped together and at the end, I just cut them off using substr.

--- RemoveVowelsFromString/main.cpp
+++ RemoveVowelsFromString/main.cpp
@@ -7,11 +7,12 @@
 //
 
 #include <iostream>
-#include <stack>
+#include <set>
 
 //For ease of access
 using namespace std;
 
+//Function that returns true if it's a vowel, false otherwise
 bool checkForVowels(char* A){
     if(*A == 'A' || *A == 'a' || *A == 'E' || *A == 'e' || *A == 'I' || *A == 'i' || *A == 'O' || *A == 'o' || *A == 'U' || *A == 'u'){
         return true;
@@ -22,31 +23,44 @@ bool checkForVowels(char* A){
 }
 string removeVowels(string word){
     int vowelCount = 0;
+    set<int> vowelIndices;
+    //Handle the reversing before doing any manipulation
     reverse(word.begin(),word.end());
     for(int i=0; i<word.length(); i++){
         //If it is a vowel and the number of characters + vowel count doesnt exceed word length
-        if(checkForVowels(&word[i]) && (i+vowelCount <= word.length())){
-            cout << "Run #" << i << " word: " << word << "\n";
+        if(checkForVowels(&word[i])){
             //Keeping track of the vowelCount to use as an index later
-            vowelCount++;
-            //Store the vowel temporarily
-            char vowel = word[i];
-            //Iterate through the rest of the array moving the characters up
-            for(int j=i; j<word.length(); j++){
-                word[j] = word[j+1];
+            if(!vowelIndices.count(i)){
+                vowelCount++;
             }
-            //Append the vowel at the end
-            word[word.length()-1] = vowel;
+            //Store the index of the vowel
+            vowelIndices.insert(i);
         }
+
+        else if(!vowelIndices.empty()){
+            //Get the first available index
+            int index = *vowelIndices.begin();
+            //Erase that index from the set
+            vowelIndices.erase(vowelIndices.begin());
+            //Insert new index into the set
+            vowelIndices.insert(i);
+            //Store the current consonant
+            char temp = word[i];
+            //Set the current consonants place = to the vowels
+            word[i] = word[index];
+            //Store the old consonant into the vowels place
+            word[index] = temp;
+        }
+        
     }
     //Cut off the vowels at the end of the string
-    word = word.substr(0,vowelCount);
+    word = word.substr(0,word.length()-vowelCount);
     return word;
 }
 
 int main() {
-    string word = "AIRPLANE";
-    string finalAnswer = removeVowels(word);
+    string word = "siiaxasiuebvalvvnueaioewhfhaljkbcsxcal";
     cout << "Initial word: " << word;
+    string finalAnswer = removeVowels(word);
     cout << "\nWord reversed without vowels: " << finalAnswer << endl;
 }
